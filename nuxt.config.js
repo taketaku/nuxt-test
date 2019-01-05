1c8787ea428894d135699d4a3fea37a95a7e7b06
taketaku <vbvbsp48@gmail.com>
2019-01-05 23:38:18 +0900

    temp

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,16 @@
 const pkg = require('./package')
+const { getConfigForKeys } = require('./lib/config.js')
+const ctfConfig = getConfigForKeys([
+  'CTF_BLOG_POST_TYPE_ID',
+  'CTF_SPACE_ID',
+  'CTF_CDA_ACCESS_TOKEN'
+])
+
+const { createClient } = require('./plugins/contentful')
+const cdaClient = createClient(ctfConfig)
 
 module.exports = {
-  mode: 'universal',
+    mode: 'universal',
 
   /*
   ** Headers of the page
@@ -49,7 +58,21 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      
+
     }
+  },
+  generate: {
+    routes() {
+      return cdaClient
+        .getEntries(ctfConfig.CTF_BLOG_POST_TYPE_ID)
+        .then(entries => {
+          return [...entries.items.map(entry => `/blog/${entry.fields.slug}`)]
+        })
+    }
+  },
+  env: {
+    CTF_SPACE_ID: ctfConfig.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: ctfConfig.CTF_CDA_ACCESS_TOKEN,
+    CTF_BLOG_POST_TYPE_ID: ctfConfig.CTF_BLOG_POST_TYPE_ID
   }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,66 +1,34 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        test-nuxt
-      </h1>
-      <h2 class="subtitle">
-        My scrumtrulescent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+  <section class="index">
+    <card
+      v-for="(post,i ) in posts"
+      :key="i"
+      :title="post.fields.title"
+      :id="post.sys.id"
+      :date="post.sys.updatedAt"
+    />
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Card from '~/components/card.vue'
+import { createClient } from '~/plugins/contentful.js'
 
+const client = createClient()
 export default {
+  transition: 'slide-left',
   components: {
-    Logo
+    Card
+  },
+  asyncData({ env, params }) {
+    return client
+      .getEntries(env.CTF_BLOG_POST_TYPE_ID)
+      .then(entries => {
+        return {
+          posts: entries.items
+        }
+      })
+      .catch(console.error)
   }
 }
 </script>
-
-<style>
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
